8dda0275cd86f70b30feb8f4ba7403960e3f71c2
Michele Costa <michcosta2211@gmail.com>
2025-01-27 12:37:27 -0700

    ignore hidden files

--- locate-file-script.py
+++ locate-file-script.py
@@ -1,21 +1,33 @@
-# Import os module
 import os
 
-# get the current working directory
-cwd = os.getcwd()
+def locate_file(cwd=None):
+    if cwd is None: # Default to the current working directory if path is not provided
+        cwd = os.getcwd()
+    file_dic = []  # List to store dictionaries about files
 
-# store list of dictionaries
-file_dic = []
+    # Walk through the directory and subdirectories
+    # underscore(_), to indicate that the loop variable "subfolders" is intentionally not being used in the loop body
+    for folder, _, files in os.walk(cwd):
+        for file in files:
+            # Skip hidden files (starting with .)
+            if file.startswith('.'):
+                continue
+            
+            file_path = os.path.join(folder, file)
+            file_info = {
+                'File': file_path,
+                'Size': os.path.getsize(file_path)
+                }
+            file_dic.append(file_info) # append the file_info to file_dic
 
-# iterate over the list of entries using in the current directory
-# create the dictionary and append to the list
-for file in os.listdir(cwd):
-    if os.path.isfile(file): 
-        file_path = os.path.join(cwd, file) 
-        file_info = {
-            'File': file_path,
-            'Size': os.path.getsize(file_path)
-        }
-        file_dic.append(file_info)
+    return file_dic # return the list of dictionary
 
-print(*file_dic, sep = "\n")
+# Usage
+if __name__ == "__main__":
+
+    user_input = input("Enter the path: ")
+    if not user_input:
+        user_input = None
+
+    response = locate_file(user_input)
+    print(*response, sep="\n")
